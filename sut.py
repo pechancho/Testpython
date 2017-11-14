2fd13509a5afbf832ffdfea2ff091b6d9980b56e
Damian <damiantello44@outlook.com>
2017-11-14 17:32:34 -0300

    Update sut.py

--- sut.py
+++ sut.py
@@ -1,34 +1,45 @@
+import unittest
+
 import math
+from sut import *
 
-def area(ancho, alto):
-    return ancho * alto
+class Test(unittest.TestCase):
 
-def saludar(nombre):
-    return "Hola " + nombre
+    def testarea(self):
+        self.assertEqual(area(2, 3),6)
+
+    def testsaludar(self):
+        self.assertEqual(saludar("damian"),"Hola damian")
+
+    def testsumar(self):
+        self.assertEqual(sumar(30, 10),40)
+
+    def testcomparar(self):
+        self.assertEqual(comparar(1,2),"A menor que B")
+    def testcomparar2(self):
+        self.assertEqual(comparar(2,1),"A mayor que B")
+    def testcomparar3(self):
+        self.assertEqual(comparar(2,2),"A y B son iguales")
+
+
+	
+    def testvalorabsoluto(self):
+        self.assertEqual(valorabsoluto(-50),50)
+        
+    def testvalorabsoluto2(self):
+        self.assertEqual(valorabsoluto(40),40)
+        
+    def testcostototal(self):
+        self.assertEqual(costototal(2, 2),"El costo total es $4")
+
     
-def sumar(a, b):
-    return a + b
-    
-def comparar(a, b):
-    if a < b:
-        return "A < B"
-    if a > b:
-        return "A > B"
-    if a == b:
-        return "son iguales"
-   
-def valorabsoluto(n):
-    if n < 0:
-        n = -n
-    return n
-    
-def costototal(producto1, producto2):
-    total = sumar(producto1, producto2)
-    return "El costo total es " + str(total)
+    def test_supercalc(self):
+        math.exp=MagicMock(return_value=2)
+        math.sqrt=MagicMock(return_value=2)
+        a=supercalc(3)
+        self.assertTrue(a==2)
 
 
-def supercalc(num):
-    exp = math.exp(num)
-    sum = exp + 2
-    sqrt = math.sqrt(sum)
-    return sqrt
+
+if __name__ == '__main__':
+    unittest.main()
